8e9a9f9f70d5c012784e87f05aae0187b0df6306
Quoc Do <quocbdo@gmail.com>
2017-09-01 18:29:34 -0700

    step 3: persist token JWT in client

---
+++ src/utils/userService.js
@@ -0,0 +1,11 @@
+import userAPI from './userAPI';
+import tokenService from './tokenService';
+
+function signup(user) {
+    return userAPI.signup(user)
+    .then(token => tokenService.setToken(token));
+}
+
+export default {
+    signup
+}
